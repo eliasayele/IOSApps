bd4bc3f45bd69048684983a68d6ba7d611d7bf39
Elias <eliasey11@gmail.com>
2021-08-31 09:32:09 +0300

    carousel for home page is added

--- Landmark/Landmark/LandmarkApp.swift
+++ Landmark/Landmark/LandmarkApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+ 
 @main
 struct LandmarkApp: App {
     
--- Landmark/Landmark/Views/PageView/PageView.swift
+++ Landmark/Landmark/Views/PageView/PageView.swift
@@ -7,24 +7,23 @@
 
 import SwiftUI
 
-struct PageView<Page:View>: View {
-    var pages:[Page]
+struct PageView<Page: View>: View {
+    var pages: [Page]
     @State private var currentPage = 0
 
     var body: some View {
-        PageViewController(pages: pages, currentPage: $currentPage)
-     }
-    
-    
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView(pages: ModelData().features.map {
-                    FeatureCard(landmark: $0) })
-                   .aspectRatio(3 / 2, contentMode: .fit)
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
-    
-    
-
 }
